014ce097163cc8fac421d984c915bafbdb8783c4
LizKamugisha <liz.kamugisha@gmail.com>
2020-05-24 00:12:13 +0300

    updated files

--- frontend/loginPage/login.js
+++ frontend/loginPage/login.js
@@ -1,19 +1,19 @@
 // Login User - trial 1
-const login = [];
-loginBtn.addEventListener ("click", (e) => {
-    e.preventDefault() 
-{
-router.post("/users/login", async (req,res) => {
-    const {name,password,role} = req.body;
-    const user = await User.findByCredentials(name,password,role);
-    if (name == " " && password == " " && role == " ")
-    {   const token = await user.generateAuthToken() 
-        console.log("Logged in successfully");
-        window.location = "dashboard.html"; 
-    return true;
-    } else {
-        (user.error)
-        alert ("Invalid username or password");
-        return false;
-    }
-}}});
+// const login = [];
+// loginBtn.addEventListener ("click", (e) => {
+//     e.preventDefault() 
+// {
+// router.post("/users/login", async (req,res) => {
+//     const {name,password,role} = req.body;
+//     const user = await User.findByCredentials(name,password,role);
+//     if (name == " " && password == " " && role == " ")
+//     {   const token = await user.generateAuthToken() 
+//         console.log("Logged in successfully");
+//         window.location = "dashboard.html"; 
+//     return true;
+//     } else {
+//         (user.error)
+//         alert ("Invalid username or password");
+//         return false;
+//     }
+// }}});
